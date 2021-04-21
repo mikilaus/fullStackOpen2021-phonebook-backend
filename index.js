f2960b059156f7278f07814b19140e760bc3074b
Miklos Komcsak <komcsakmiklos@gmail.com>
2021-04-21 16:20:16 +0200

    Phonebook backend step6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -38,6 +40,58 @@ app.get("/info", (request, response) => {
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  let id = Number(request.params.id);
+  console.log(id);
+  let person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  let id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const genId = () => {
+  const id = Math.floor(Math.random() * 1000);
+  return id;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  let duplicateName = persons.find(
+    (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  );
+  if (duplicateName) {
+    return response.status(400).json({
+      error: "Name alrady exist in the phonebook",
+    });
+  }
+
+  const person = {
+    id: genId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
