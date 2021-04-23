446cbb6a977162bbdbd4c681f2fb70894290c9aa
Miklos Komcsak <komcsakmiklos@gmail.com>
2021-04-23 13:04:24 +0200

    Phonebook backend step8

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+morgan.token("person", (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 let persons = [
   {
